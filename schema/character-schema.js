c8a4d36c4a46de89d9056942cfdc4c95ea257d1c
krishnakandula <krishna.c.kandula@gmail.com>
2017-04-22 22:34:02 -0500

    Add sql and cols dir

---
+++ schema/character-schema.js
@@ -0,0 +1,13 @@
+//Contains the schema for the character relation
+
+const COLS = {
+    ID: 'character_id',
+    NAME: 'character_name',
+    SEX: 'sex',
+    EXP: 'experience_points',
+    HP: 'hit_points'
+};
+
+module.exports = {
+    COLS
+};
