0827044ff1ee17a243f9577f79cb03a49ec2b688
MandyJCho <chomandy96@gmail.com>
2017-04-26 18:24:15 -0500

    Created armory related schemas

---
+++ schema/armory_armor_schema.js
@@ -0,0 +1,14 @@
+// Contains armory_armor relation 
+
+const TABLE_NAME = "armory_armor";
+
+const COL = {
+    ARMOR_ID: "armor_id",
+    ARMORY_ID: "armory_id",
+    MARKET_ID: "market_id",
+}
+
+module.exports = {
+    TABLE_NAME,
+    COL
+};
---
+++ schema/armory_weapon_schema.js
@@ -0,0 +1,14 @@
+_// Contains armory weapon schema
+
+const TABLENAME = "armory_weapon";
+
+const COL = {
+    TOOLS_ID : "weapons_id",
+    ARMORY_ID : "armory_id",
+    MARKET_ID: "market_id"
+};
+
+module.exports = {
+    TABLENAME,
+    COL
+};
