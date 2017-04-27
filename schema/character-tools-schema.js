93046b6a2fa9f66c43e23d0bb6698f7dbb3b2dab
MandyJCho <chomandy96@gmail.com>
2017-04-27 10:54:34 -0500

    Create schemas for character inventory items

---
+++ schema/character-tools-schema.js
@@ -0,0 +1,16 @@
+// Contains schema for character_tools
+const INVENTORY = require("./inventory-schema");
+const TOOLS = require("./tools-schema");
+
+const TABLE_NAME = "character_tools";
+
+const COLS = {
+    TOOLS_ID: TOOLS.COLS.TOOLS_ID,
+    CHARACTER_ID: INVENTORY.COLS.CHARACTER_ID,
+    INVENTORY_ID: INVENTORY.COLS.INVENTORY_ID
+};
+
+module.exports = {
+    TABLE_NAME,
+    COLS
+};
---
+++ schema/character-weapon-schema.js
@@ -0,0 +1,16 @@
+// Contains schema for character_weapon
+const INVENTORY = require("./inventory-schema");
+const WEAPONS = require("./weapons-schema");
+
+const TABLE_NAME = "character_weapon";
+
+const COLS = {
+    CHARACTER_ID: INVENTORY.COLS.CHARACTER_ID,
+    WEAPON_ID: WEAPONS.COLS.WEAPONS_ID,
+    INVENTORY_ID: INVENTORY.COLS.INVENTORY_ID
+};
+
+module.exports = {
+    TABLE_NAME,
+    COLS
+};
