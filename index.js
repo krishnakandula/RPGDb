f639915b80f11aea5f5199a62fb80bee79d32343
krishnakandula <krishna.c.kandula@gmail.com>
2017-04-27 18:00:24 -0500

    Remove sample db push in index

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const {client} = require("./db");
-const queries = require("./sql/queries");
 
 //Connect to database
 client.connect(err => {
     if (err) throw err;
 
-    //Execute queries
+    //Execute queries here
     console.log("Connection successful");
 
-    let id = 57483;
-    let name = "";
-    let sex = true;
-    let hp = 100;
-    let exp = 150;
-
-    let values = [id, name, sex, exp, hp];
-    queries.sample.values = values;
-
-    client
-        .query(queries.sample)
-        .then(result => {
-            console.log(result);
-        })
-        .catch(err => {
-            console.error(err);
-        }).then(() => {
-        //Exit cleanly
-        process.exit(0);
-    });
 });
