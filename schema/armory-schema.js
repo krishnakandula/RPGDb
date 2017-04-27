3ed3d9cd11ee21328214070839c9c200749fb30d
MandyJCho <chomandy96@gmail.com>
2017-04-27 10:27:09 -0500

    Match attribute ordering with table

--- schema/armory-schema.js
+++ schema/armory-schema.js
@@ -4,8 +4,8 @@ const MARKET = require("./market_schema");
 const TABLE_NAME = "characters";
 
 const COLS = {
-    ARMORY_ID: "armory_id",
     MARKET_ID: MARKET.COL.MARKET_ID,
+    ARMORY_ID: "armory_id",
     BLACKSMITH: "blacksmith",
 }
   
