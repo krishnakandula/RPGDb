fb6e2fc5908a665bced159c5f2cf3ba245f90b9d
MandyJCho <chomandy96@gmail.com>
2017-04-27 09:40:34 -0500

    Corrected create table statements

--- FlockCreateGuild.sql
+++ FlockCreateGuild.sql
@@ -1,5 +1,5 @@
 CREATE TABLE CHARACTER (
-	Character_id INT NOT NULL
+	Character_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Character_id)
 	,Character_name VARCHAR(30) NOT NULL
 	,Sex BOOLEAN
@@ -7,37 +7,41 @@ CREATE TABLE CHARACTER (
 	,Hit_points INT DEFAULT 0
 );
 CREATE TABLE ARMORY (
-	Market_id INT NOT NULL
-	,Armory_id INT NOT NULL
+	Market_id VARCHAR(64) NOT NULL
+	,Armory_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Market_id, Armory_id)
 	,Blacksmith VARCHAR(30)
 	,FOREIGN KEY (Market_id) REFERENCES MARKET(Market_id)
 );
 CREATE TABLE ARMOR (
-	Armor_id INT NOT NULL
+	Armor_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Armor_id)
 );
 CREATE TABLE TOOLS (
-	Tools_id INT NOT NULL
+	Tools_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Tools_id)
 );
 CREATE TABLE WEAPONS (
-	Weapons_id INT NOT NULL
+	Weapons_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Weapons_id)
 );
 CREATE TABLE BARRACKS (
-	Barracks_id INT NOT NULL
+	Barracks_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Barracks_id)
 	,Barracks_name VARCHAR(30)
 );
 CREATE TABLE CAFETERIA (
-	Barracks_id INT NOT NULL
-	,PRIMARY KEY (Barracks_id)
+	Barracks_id VARCHAR(64) NOT NULL
+	,Cafeteria_id VARCHAR(64) NOT NULL
+	,PRIMARY KEY (
+		Barracks_id
+		,Cafeteria_id
+		)
 	,Head_chef VARCHAR(30)
 	,FOREIGN KEY (Barracks_id) REFERENCES BARRACKS(Barracks_id)
 );
 CREATE TABLE COLOSSEUM (
-	Mission_id INT NOT NULL
+	Mission_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Mission_id) REFERENCES MISSION(Mission_id)
 	,PRIMARY KEY (Mission_id)
 	,Admission_fee INT NOT NULL DEFAULT 100
@@ -45,8 +49,8 @@ CREATE TABLE COLOSSEUM (
 );
 CREATE TABLE GENERAL_STORE (
 	Store_keeper VARCHAR(30)
-	,Store_id INT NOT NULL
-	,Market_id INT NOT NULL
+	,Store_id VARCHAR(64) NOT NULL
+	,Market_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Market_id) REFERENCES MARKET(Market_id)
 	,PRIMARY KEY (
 		Market_id,
@@ -54,13 +58,17 @@ CREATE TABLE GENERAL_STORE (
 );
 CREATE TABLE INFIRMARY (
 	Head_nurse VARCHAR(30)
-	,Barracks_id INT NOT NULL
+	,Barracks_id VARCHAR(64) NOT NULL
+	,Infirmary_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Barracks_id) REFERENCES BARRACKS(Barracks_id)
-	,PRIMARY KEY (Barracks_id)
+	,PRIMARY KEY (
+		Barracks_id
+		,Infirmary_id
+		)
 );
 CREATE TABLE INVENTORY (
-	Inventory_id INT NOT NULL
-	,Character_id INT NOT NULL
+	Inventory_id VARCHAR(64) NOT NULL
+	,Character_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id) REFERENCES CHARACTER(Character_id)
 	,PRIMARY KEY (
 		Inventory_id
@@ -69,31 +77,31 @@ CREATE TABLE INVENTORY (
 	,Gold INT DEFAULT 100
 );
 CREATE TABLE MAGICIAN (
-	Character_id INT NOT NULL
+	Character_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id) REFERENCES CHARACTER(Character_id)
 	,PRIMARY KEY (Character_id)
 	,Magic_Points INT DEFAULT 100
 	,Type_flag_1 BOOLEAN DEFAULT FALSE
 );
 CREATE TABLE MARKET (
-	Market_id INT NOT NULL
+	Market_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Market_id)
 	,Market_name VARCHAR(30)
-	,Character_id INT DEFAULT NULL
+	,Character_id VARCHAR(64) DEFAULT NULL
 	,FOREIGN KEY (Character_id) REFERENCES CHARACTER(Character_id)
 );
 CREATE TABLE MISSION (
-	Mission_id INT NOT NULL
+	Mission_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Mission_id)
 );
 CREATE TABLE PARTY (
 	Party_name VARCHAR(30) NOT NULL
-    ,Party_id INT NOT NULL
+    ,Party_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Party_id)
 );
 CREATE TABLE PET (
-	Pet_id INT NOT NULL
-	,Character_id INT NOT NULL
+	Pet_id VARCHAR(64) NOT NULL
+	,Character_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id) REFERENCES CHARACTER(Character_id)
 	,PRIMARY KEY (
 		Pet_id
@@ -103,13 +111,17 @@ CREATE TABLE PET (
 	,Health_points INT DEFAULT 100
 );
 CREATE TABLE QUARTERS (
-	Barracks_id INT NOT NULL
-	,PRIMARY KEY (Barracks_id)
+	Barracks_id VARCHAR(64) NOT NULL
+	,Quarters_id VARCHAR(64) NOT NULL
+	,PRIMARY KEY (
+		Barracks_id
+		,Quarters_id
+		)
 	,Quarter_master VARCHAR(30)
 	,FOREIGN KEY (Barracks_id) REFERENCES BARRACKS(Barracks_id)
 );
 CREATE TABLE QUEST (
-	Mission_id INT NOT NULL
+	Mission_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Mission_id)
 	,Quest_name VARCHAR(30)
 	,Quest_giver VARCHAR(30)
@@ -118,10 +130,10 @@ CREATE TABLE QUEST (
 	,FOREIGN KEY (Mission_id) REFERENCES MISSION(Mission_id)
 );
 CREATE TABLE REWARDS (
-	Rewards_id INT NOT NULL
+	Rewards_id VARCHAR(64) NOT NULL
 	,Gold INT DEFAULT 100
 	,Experience_points INT DEFAULT 100
-	,Mission_id INT NOT NULL
+	,Mission_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Mission_id) REFERENCES MISSION(Mission_id)
 	,PRIMARY KEY (
 		Rewards_id
@@ -129,23 +141,23 @@ CREATE TABLE REWARDS (
 		)
 );
 CREATE TABLE FOOD (
-	Food_id INT NOT NULL
+	Food_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Food_id)
 );
 CREATE TABLE POTION (
-	Potion_id INT NOT NULL
+	Potion_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Potion_id)
 );
 CREATE TABLE WARRIOR (
-	Warrior_id INT NOT NULL
+	Warrior_id VARCHAR(64) NOT NULL
 	,PRIMARY KEY (Warrior_id)
 	,Stamina_points INT DEFAULT 100
 	,Type_flag_1 BOOLEAN DEFAULT FALSE
 	,Type_flag_2 BOOLEAN DEFAULT FALSE
 );
 CREATE TABLE CHARACTER_SKILL (
-	Skill_id INT NOT NULL
-	,Character_id INT
+	Skill_id VARCHAR(64) NOT NULL
+	,Character_id VARCHAR(64)
 	,FOREIGN KEY (Character_id) REFERENCES CHARACTER(Character_id)
 	,PRIMARY KEY (
 		Skill_id
@@ -153,10 +165,10 @@ CREATE TABLE CHARACTER_SKILL (
 		)
 );
 CREATE TABLE CHARACTER_FOOD (
-	Food_id INT NOT NULL
+	Food_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Food_id) REFERENCES FOOD(Food_id)
-	,Character_id INT
-	,Inventory_id INT NOT NULL
+	,Character_id VARCHAR(64)
+	,Inventory_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id, Inventory_id) REFERENCES INVENTORY(Character_id, Inventory_id)
 	,CONSTRAINT Character_Food_Key PRIMARY KEY (
 		Food_id
@@ -165,10 +177,10 @@ CREATE TABLE CHARACTER_FOOD (
 		)
 );
 CREATE TABLE STORE_FOOD (
-	Food_id INT NOT NULL
+	Food_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Food_id) REFERENCES FOOD(Food_id)
-	,General_store_id INT NOT NULL
-	,Market_id INT NOT NULL
+	,General_store_id VARCHAR(64) NOT NULL
+	,Market_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (General_store_id, Market_id) REFERENCES GENERAL_STORE(Store_id, Market_id)
 	,CONSTRAINT Store_food_key PRIMARY KEY (
 		Food_id
@@ -177,10 +189,10 @@ CREATE TABLE STORE_FOOD (
 		)
 );
 CREATE TABLE CHARACTER_POTION (
-	Potion_id INT NOT NULL
+	Potion_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Potion_id) REFERENCES POTION(Potion_id)
-	,Character_id INT NOT NULL
-	,Inventory_id INT NOT NULL
+	,Character_id VARCHAR(64) NOT NULL
+	,Inventory_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id, Inventory_id) REFERENCES INVENTORY(Character_id, Inventory_id)
 	,CONSTRAINT Character_Potion_Key PRIMARY KEY (
 		Potion_id
@@ -189,10 +201,10 @@ CREATE TABLE CHARACTER_POTION (
 		)
 	);
 CREATE TABLE STORE_POTION (
-	Potion_id INT NOT NULL
+	Potion_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Potion_id) REFERENCES POTION(Potion_id)
-	,General_store_id INT NOT NULL
-	,Market_id INT NOT NULL
+	,General_store_id VARCHAR(64) NOT NULL
+	,Market_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (General_store_id, Market_id) REFERENCES GENERAL_STORE(Store_id, Market_id)
 	,CONSTRAINT Store_Potion_Key PRIMARY KEY (
 		Potion_id
@@ -201,10 +213,10 @@ CREATE TABLE STORE_POTION (
 		)
 	);
 CREATE TABLE ARMORY_ARMOR (
-	Armor_id INT NOT NULL
+	Armor_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Armor_id) REFERENCES ARMOR(Armor_id)
-	,Armory_id INT NOT NULL
-	,Market_id INT NOT NULL
+	,Armory_id VARCHAR(64) NOT NULL
+	,Market_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Armory_id, Market_id) REFERENCES ARMORY(Armory_id, Market_id)
 	,CONSTRAINT Armory_Armor_Key PRIMARY KEY (
 		Armor_id
@@ -213,10 +225,10 @@ CREATE TABLE ARMORY_ARMOR (
 		)
 	);
 CREATE TABLE CHARACTER_ARMOR (
-	Armor_id INT NOT NULL
+	Armor_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Armor_id) REFERENCES ARMOR(Armor_id)
-	,Character_id INT NOT NULL
-	,Inventory_id INT NOT NULL
+	,Character_id VARCHAR(64) NOT NULL
+	,Inventory_id VARCHAR(64) NOT NULL
     ,FOREIGN KEY (Character_id, Inventory_id) REFERENCES INVENTORY(Character_id, Inventory_id)
 	,CONSTRAINT Character_Armor_key PRIMARY KEY (
 		Armor_id
@@ -225,9 +237,9 @@ CREATE TABLE CHARACTER_ARMOR (
 		)
 );
 CREATE TABLE CHARACTERS_JOINS_A_PARTY (
-	Character_id INT NOT NULL
+	Character_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id) REFERENCES CHARACTER(Character_id)
-	,Party_id INT NOT NULL
+	,Party_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Party_id) REFERENCES PARTY(Party_id)
 	,PRIMARY KEY (
 		Character_id
@@ -235,9 +247,9 @@ CREATE TABLE CHARACTERS_JOINS_A_PARTY (
 		)
 );
 CREATE TABLE PARTY_PARTICIPATES_IN_MISSION (
-	Party_id INT NOT NULL
+	Party_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Party_id) REFERENCES PARTY(Party_id)
-	,Mission_id INT NOT NULL
+	,Mission_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Mission_id) REFERENCES MISSION(Mission_id)
 	,PRIMARY KEY (
 		Party_id
@@ -245,10 +257,10 @@ CREATE TABLE PARTY_PARTICIPATES_IN_MISSION (
 		)
 );
 CREATE TABLE ARMORY_TOOL (
-	Tools_id INT NOT NULL
+	Tools_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Tools_id) REFERENCES TOOLS(Tools_id)
-	,Armory_id INT NOT NULL
-	,Market_id INT NOT NULL
+	,Armory_id VARCHAR(64) NOT NULL
+	,Market_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Armory_id, Market_id) REFERENCES ARMORY(Armory_id, Market_id)
 	,CONSTRAINT Armory_Tool_Key PRIMARY KEY (
 		Tools_id
@@ -257,10 +269,10 @@ CREATE TABLE ARMORY_TOOL (
 		)
 	);
 CREATE TABLE CHARACTER_TOOL (
-	Tools_id INT NOT NULL
+	Tools_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Tools_id) REFERENCES TOOLS(Tools_id)
-	,Character_id INT NOT NULL
-	,Inventory_id INT NOT NULL
+	,Character_id VARCHAR(64) NOT NULL
+	,Inventory_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id, Inventory_id) REFERENCES INVENTORY(Character_id, Inventory_id)
 	,CONSTRAINT Character_Tool_Key PRIMARY KEY (
 		Tools_id
@@ -269,10 +281,10 @@ CREATE TABLE CHARACTER_TOOL (
 		)
 	);
 CREATE TABLE ARMORY_WEAPON (
-	Weapons_id INT NOT NULL
+	Weapons_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Weapons_id) REFERENCES WEAPONS(Weapons_id)
-	,Armory_id INT NOT NULL
-	,Market_id INT NOT NULL
+	,Armory_id VARCHAR(64) NOT NULL
+	,Market_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Armory_id, Market_id) REFERENCES ARMORY(Armory_id, Market_id)
 	,CONSTRAINT Armory_Weapon_Key PRIMARY KEY (
 		Weapons_id
@@ -281,10 +293,10 @@ CREATE TABLE ARMORY_WEAPON (
 		)
 	);
 CREATE TABLE CHARACTER_WEAPON (
-	Character_id INT NOT NULL
-	,Weapons_id INT NOT NULL
+	Character_id VARCHAR(64) NOT NULL
+	,Weapons_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Weapons_id) REFERENCES WEAPONS(Weapons_id)
-	,Inventory_id INT NOT NULL
+	,Inventory_id VARCHAR(64) NOT NULL
 	,FOREIGN KEY (Character_id, Inventory_id) REFERENCES INVENTORY(Character_id, Inventory_id)
 	,CONSTRAINT Character_Weapon_Key PRIMARY KEY (
 		Weapons_id
