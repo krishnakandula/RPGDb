ec875f3606aa7f62a431a9b327de60edca1ea388
MandyJCho <chomandy96@gmail.com>
2017-04-28 13:53:34 -0500

    Add insert statement for character armor

--- schema/character-armor-schema.js
+++ schema/character-armor-schema.js
@@ -1,16 +1,31 @@
 // Contains schema for character_armor 
 const INVENTORY = require("./inventory-schema");
 const ARMOR = require("./armor-schema");
+const generator = require("./../generator");
 
 const TABLE_NAME = "character_armor";
-
-// let insertCharacterArmorStatement = `INSERT INTO ${}`
 
 const COLS = {
     ARMOR_ID: ARMOR.COLS.ARMOR_ID,
     CHARACTER_ID: INVENTORY.COLS.CHARACTER_ID,
     INVENTORY_ID: INVENTORY.COLS.INVENTORY_ID
 };
+
+let insertStatement = `INSERT INTO ${TABLE_NAME} (${COLS.ARMOR_ID} ${COLS.CHARACTER_ID}, ${COLS.INVENTORY_ID}) VALUES ($1, $2, $3)`;
+
+let generateValues= () => {
+    let armorId;
+    let characterId;
+    let inventoryId;
+
+    return [armorId, characterId, inventoryId];
+}
+
+let characterArmor = {
+    name: `${TABLE_NAME} insert`,
+    text: insertStatement,
+    values: generateStatement;
+}
 
 module.exports = {
     TABLE_NAME,
