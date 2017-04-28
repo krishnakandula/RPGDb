af4e89d99a4513f5092fc8b29d598a0d2c6fb1e3
MandyJCho <chomandy96@gmail.com>
2017-04-28 12:54:19 -0500

    Add insert statement for food

--- schema/potion-schema.js
+++ schema/potion-schema.js
@@ -1,11 +1,19 @@
 //Contains the schema for the potion relation
+const generator = require("./../generator");
 const TABLE_NAME = "potion";
 
 const COLS = {
 	POTION_ID: "potion_id",
 }
 
+let insertFoodStatement = `INSERT INTO ${TABLE_NAME} (${COLS.POTION_ID}) VALUES ($1)`;
+
+let potion = {
+  name: `${TABLE_NAME} insert`,
+  text: insertFoodStatement,
+  values: [generator.generateId()]
+}
+
 module.exports = {
-  TABLE_NAME,
-  COLS
+  potion
 };
