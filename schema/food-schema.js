f08daa011e0d017e54bec5483151be06927d2eb4
shawyanjafari <shawyanjafari@gmail.com>
2017-04-28 12:31:24 -0500

    add insert statement for food

--- schema/food-schema.js
+++ schema/food-schema.js
@@ -1,11 +1,23 @@
 //Contains the schema for the food relation
+const generator = require("./../generator");
+
 const TABLE_NAME = "food";
 
 const COLS = {
 	FOOD_ID: "food_id",
 }
 
+let insertFoodStatement = `INSERT INTO ${TABLE_NAME} 
+                                (${COLS.FOOD_ID})
+                                VALUES ($1)`;
+
+let food = {
+	name: `${TABLE_NAME} insert`,
+	text: insertFoodStatement,
+	values: [generator.generatorId()]
+};
+
+
 module.exports = {
-  TABLE_NAME,
-  COLS
+  food
 };
