4794d966e1a95a0d49c6bd4b3c2c24821c2fb6d1
MandyJCho <chomandy96@gmail.com>
2017-04-27 21:57:24 -0500

    Add tool insert statement

--- schema/armor-schema.js
+++ schema/armor-schema.js
@@ -1,11 +1,23 @@
 // Contains the armor schema 
+const generator = require("./../generator");
+
 const TABLE_NAME = "armor";
 
 const COLS = {
     ARMOR_ID: "armor_id",
 }
 
+let insertArmorStatement = `INSERT INTO ${TABLE_NAME}
+                                (${COLS.ARMOR_ID}) 
+                                VALUES ($1)`;
+
+let armor = {
+    name: `${TABLE_NAME} insert`,
+    text: insertArmorStatement,
+    values: [generator.generateId()]
+};
+
+
 module.exports = {
-    TABLE_NAME,
-    COLS
+    armor
 };
--- schema/tools-schema.js
+++ schema/tools-schema.js
@@ -1,11 +1,22 @@
 // Contains tools schema
-const TABLENAME = "tools";
+const generator = require("./../generator");
+
+const TABLE_NAME = "tools";
 
 const COLS = {
     TOOLS_ID: "tools_id",
 };
 
+let insertToolsStatement = `INSERT INTO ${TABLE_NAME} 
+                            (${COLS.TOOLS_ID})
+                            VALUES ($1)`;
+
+let tools = {
+    name: `${TABLE_NAME} insert`,
+    text: insertToolsStatement,
+    value: [generator.generateId()]
+};
+
 module.exports = {
-    TABLENAME,
-    COLS,
+   tools
 };
