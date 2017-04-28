35fcf356051cea80770f9157f945193107d30cb3
krishnakandula <krishna.c.kandula@gmail.com>
2017-04-28 12:37:56 -0500

    Change character-schema to update every time

--- schema/character-schema.js
+++ schema/character-schema.js
@@ -1,3 +1,6 @@
+const generator = require('./../generator');
+const {character} = require('./../data/constants');
+
 //Contains the schema for the character relation
 const TABLE_NAME = "character";
 
@@ -9,7 +12,31 @@ const COLS = {
     HP: "hit_points"
 };
 
+let insertCharacterStatement =
+    `INSERT INTO ${TABLE_NAME} (${COLS.ID}, ${COLS.NAME}, ${COLS.SEX}, ${COLS.EXP}, ${COLS.HP}) VALUES ($1, $2, $3, $4, $5)`;
+
+let generateStatement = () => {
+    let id = generator.generateId();
+    let name = generator.generateRandomName();
+    let sex = generator.generateRandomSex();
+    let exp = generator.generateRandomNumber(character.MIN_EXP, character.MAX_EXP);
+    let hp = generator.generateRandomNumber(character.MIN_HP, character.MAX_HP);
+
+    return [id, name, sex, exp, hp];
+}
+
+let preparedStatement = {
+    name: `${TABLE_NAME} insert`,
+    text: insertCharacterStatement,
+    values: []
+};
+
+let generate = () => {
+    preparedStatement.values = generateStatement();
+    return preparedStatement;
+}
+
 module.exports = {
     TABLE_NAME,
-    COLS
+    generate
 };
