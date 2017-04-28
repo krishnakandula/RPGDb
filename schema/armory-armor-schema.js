3d734ae4566a843c37908b29876917cd99fa37b6
MandyJCho <chomandy96@gmail.com>
2017-04-27 22:07:42 -0500

    Add insert statement for weapon and armory_armor

--- schema/armory-armor-schema.js
+++ schema/armory-armor-schema.js
@@ -1,6 +1,7 @@
 // Contains armory_armor relation 
 const ARMORY = require("./armory-schema");
 const ARMOR = require("./armor-schema");
+const generator = require("./../generator")
 
 const TABLE_NAME = "armory_armor";
 
@@ -8,6 +9,16 @@ const COLS = {
     ARMOR_ID: ARMOR.COLS.ARMOR_ID,
     ARMORY_ID : ARMORY.COLS.ARMORY_ID,
     MARKET_ID: ARMORY.COLS.MARKET_ID
+}
+
+let insertArmoryArmorStatement = `INSERT INTO ${TABLE_NAME} 
+                                    (${COLS.ARMOR_ID} ${COLS.ARMORY_ID} ${COLS.MARKET_ID})
+                                    VALUES ($1, $2, $3)`;
+
+let armory_armor = {
+    name: `${TABLE_NAME} insert`,
+    text: insertArmoryArmorStatement,
+    value: 
 }
 
 module.exports = {
--- schema/weapons-schema.js
+++ schema/weapons-schema.js
@@ -1,11 +1,22 @@
 // Contains weapons schema
+const generator = require("./../generator");
+
 const TABLE_NAME = "weapons";
 
 const COLS = {
     WEAPONS_ID : "weapons_id",
 };
 
+let insertWeaponsStatement = `INSERT INTO ${TABLE_NAME} 
+                                (${COLS.WEAPONS_ID})
+                                VALUES ($1)`;
+
+let weapons = {
+    name: `${TABLE_NAME} insert`,
+    text: insertWeaponsStatement,
+    values: [generator.generateId()]
+};
+
 module.exports = {
-    TABLE_NAME,
-    COLS
+    weapons
 };
