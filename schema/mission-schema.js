ccc5daf238f8200363768add3b674bff45c1554f
shawyanjafari <shawyanjafari@gmail.com>
2017-04-28 17:22:32 -0500

    fix mission schema

--- schema/mission-schema.js
+++ schema/mission-schema.js
@@ -1,11 +1,35 @@
 // Contains schema for mission
+const generator = require("./../generator")
 const TABLE_NAME = "mission";
 
 const COLS = {
     MISSION_ID: "mission_id",
 };
 
+let tuples = [];
+
+let insertMissionStatement = `INSERT INTO ${TABLE_NAME} (${COLS.MISSION_ID}) VALUES ($1)`;
+
+let generateValues = () => {
+	let mission_id = generator.generateId();
+};
+
+let preparedStatement = {
+	name: `${TABLE_NAME} insert`,
+	text: insertMissionStatement,
+	values: []
+};
+
+let generate = () => {
+    preparedStatement.values = generateStatement();
+    tuples.push(preparedStatement);
+    return preparedStatement;
+}
+
 module.exports = {
     TABLE_NAME,
-    COLS
+    tuples,
+    COLS,
+    generate
 };
+
