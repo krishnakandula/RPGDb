e6b910fd173fc92051f7839af691e3d04bd65989
shawyanjafari <shawyanjafari@gmail.com>
2017-04-28 17:32:08 -0500

    fix rewards schema

--- schema/rewards-schema.js
+++ schema/rewards-schema.js
@@ -1,6 +1,6 @@
 // Contains schema for rewards 
 const MISSION = require("./mission-schema");
-
+const generator = require("./../generator");
 const TABLE_NAME = "rewards";
 
 const COLS = {
@@ -10,7 +10,33 @@ const COLS = {
     MISSION_ID: MISSION.COLS.MISSION_ID
 };
 
+let tuples = [];
+
+let insertRewardsStatement = `INSERT INTO ${TABLE_NAME} (${COLS.REWARDS_ID}, ${COLS.GOLD},
+${COLS.EXP},${COLS.MISSION_ID},) VALUES ($1,$2,$3,$4)`;
+
+let generateValues = () => {
+	let rewards_id = generator.generateId();
+	let gold = generate.generateRandomNumbers(10,500);
+	let exp = generate.generateRandomNumbers(1,3);
+	let MISSION_ID = MISSION.tuples[generate.generateRandomNumbers(10,800)].values[0];
+};
+
+let preparedStatement = {
+	name: `${TABLE_NAME} insert`,
+	text: insertRewardsStatement,
+	values: []
+};
+
+let generate = () => {
+    preparedStatement.values = generateStatement();
+    tuples.push(preparedStatement);
+    return preparedStatement;
+}
+
 module.exports = {
     TABLE_NAME,
-    COLS
+    COLS,
+    generate,
+    tuple
 };
