cac79531feafff7627d4ce6f54bf5c2785e7031b
MandyJCho <chomandy96@gmail.com>
2017-04-28 20:54:30 -0500

    Add remaining query statements

--- sql/FlockQueryExamples.sql
+++ sql/FlockQueryExamples.sql
@@ -2,7 +2,7 @@
 SELECT COUNT(*) FROM CHARACTER;
 
 /* Show listing of all male characters */
-SELECT character_name, character_id
+SELECT character_name, character_id, sex
     FROM CHARACTER
     WHERE sex = TRUE;
 
@@ -11,4 +11,17 @@ SELECT *
     FROM CHARACTER, GENERAL_STORE
     WHERE CHARACTER.market_id = GENERAL_STORE.market_id;
 
-/* Value of of an occurrence */
+/* Value of of how much exp gained for a mission */
+SELECT (AVG(REWARDS.gold))
+        / ((SELECT COUNT(*) FROM CHARACTER) * ((SELECT COUNT(*) FROM PARTY) / 2)) "EXP GAINED FROM MISSION"
+    FROM REWARDS;
+
+/* Show mission schedule for a party */
+SELECT * 
+    FROM MISSION
+    WHERE party_id IN
+    (SELECT party_id 
+        FROM PARTY
+        LIMIT 1 OFFSET 14
+    ) ORDER BY fight_date
+    LIMIT 6;
