c3efe31d9899fb3536049ed2e4f6cabb346a2d0b
krishnakandula <krishna.c.kandula@gmail.com>
2017-04-28 19:35:43 -0500

    Add date to mission-schema and create sql script

--- schema/mission-schema.js
+++ schema/mission-schema.js
@@ -6,18 +6,21 @@ const TABLE_NAME = "mission";
 
 const COLS = {
     MISSION_ID: "mission_id",
-    PARTY_ID: PARTY.COLS.PARTY_ID
+    PARTY_ID: PARTY.COLS.PARTY_ID,
+    DATE: "fight_date_and_time"
 };
 
 let tuples = [];
 
-let insertMissionStatement = `INSERT INTO ${TABLE_NAME} (${COLS.MISSION_ID}, ${COLS.PARTY_ID}) VALUES ($1,$2)`;
+let insertMissionStatement = `INSERT INTO ${TABLE_NAME} (${COLS.MISSION_ID}, ${COLS.PARTY_ID}, ${COLS.DATE}) VALUES ($1, $2, $3)`;
 
 let generateStatement = () => {
 	let mission_id = generator.generateId();
     let party_id = PARTY.tuples[generator.generateRandomNumber(0, NUM_PARTIES)].values[1];
-
-    return [mission_id, party_id];
+    let jsDate = new Date();
+    jsDate.setDate(jsDate.getDate() + generator.generateRandomNumber(1, 20));
+    let sqlDate = jsDate.toISOString().slice(0, 19).replace('T', ' ');
+    return [mission_id, party_id, sqlDate];
 };
 
 let preparedStatement = {
